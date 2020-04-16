faf40f7b3c8ba808aa3134a9a2d7d985a00fad6c
Ronak <ronak.vadhaiya@somaiya.edu>
2020-04-16 14:05:06 +0530

    cast1

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants'
+import Home from './screens/Home'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Home />
     </View>
   );
 }
@@ -12,7 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    marginTop:Constants.statusBarHeight,
+    backgroundColor: '#eee',
     alignItems: 'center',
     justifyContent: 'center',
   },
